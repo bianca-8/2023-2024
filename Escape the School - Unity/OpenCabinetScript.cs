ade349e7796e2311c548dd429de58c845224c0d3
Bianca <130195861+bianca-8@users.noreply.github.com>
2023-11-25 00:02:01 -0500

    Add files via upload

---
+++ Escape the School - Unity/OpenCabinetScript.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class PillowScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    void OnMouseDown()
+    {          
+        this.gameobject.SetActive(false);
+
+        
+    } 
+}
